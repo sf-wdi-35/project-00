2dfeaa42d673521fe1583c4ffb6b70ff54c8adb5
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-01 17:01:36 -0800

    Implemented logic for two players to move and alert winning condition

--- js/app.js
+++ js/app.js
@@ -1,15 +1,41 @@
 $(document).ready(function() {
-	var position = {
-		top: 0,
-		left: 50
-	}
-	$(document).keypress(function move() {
-	
-			$("div").offset(position);
-			position.left +=10;
+	var player1 = {
+		top: $("#player1").offset().top,
+		left: $("#player1").offset().left,
+		keyCode: 74 //j
 
-			console.log("i'm running!")
-		
+	}
+
+	var player2 = {
+		top: $("#player2").offset().top,
+		left: $("#player2").offset().left,
+		keyCode: 68 //d
+	}
+
+	var finish = {
+		top: $("#finish").offset().top,
+		left: $("#finish").offset().left
+	}
+
+	//Keydown event: When player1 presses j key or player2 presses d key, their left position will increase by 20px
+	$(document).keydown(function move(e) {
+			if (e.keyCode == player1.keyCode) {
+				$("#player1").offset({top: player1.top, left: player1.left += 20});
+			} 
+			if(e.keyCode == player2.keyCode) {
+				$("#player2").offset({top: player2.top, left: player2.left +=20});
+			}
+
+			if (player1.left == finish.left && player2.left == finish.left) {
+					alert("it's a tie"); //create a tie window because the players don't actually move simultaneously.
+			} else if (player1.left == finish.left){
+				alert("player 1 is the winner")
+			} else if (player2.left == finish.left) {
+				alert("player 2 is the winner")
+			}
 	})
+
+	//Win condition: when players' left position exceeds the left position of a finish line, the player game stops.
+
 	
 })
